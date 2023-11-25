412329644e8a48acc16fc0318c7438f5b0fb47b3
felipeViana <felipe.140492@gmail.com>
2023-11-25 17:31:18 -0300

    add start button

--- src/index.js
+++ src/index.js
@@ -1,45 +1,68 @@
-import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
+import Phaser from "phaser";
+import logoImg from "./assets/logo.png";
 
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
-        super();
-    }
+import startButtonImg from "./assets/start-button.png";
 
-    preload ()
-    {
-        //  This is an example of a bundled image:
-        this.load.image('logo', logoImg);
+class MyGame extends Phaser.Scene {
+  constructor() {
+    super();
+  }
 
-        //  This is an example of loading a static image from the public folder:
-        this.load.image('background', 'assets/bg.jpg');
-    }
-      
-    create ()
-    {
-        this.add.image(400, 300, 'background');
+  preload() {
+    //  This is an example of a bundled image:
+    this.load.image("logo", logoImg);
+    this.load.image("startButton", startButtonImg);
 
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
+    //  This is an example of loading a static image from the public folder:
+    this.load.image("background", "assets/bg.jpg");
+  }
+
+  create() {
+    this.add.image(400, 300, "background");
+
+    const logo = this.add.image(400, 150, "logo");
+
+    this.tweens.add({
+      targets: logo,
+      y: 450,
+      duration: 2000,
+      ease: "Power2",
+      yoyo: true,
+      loop: -1,
+    });
+
+    const startButton = this.add
+      .sprite(300, 300, "startButton")
+      .setInteractive()
+      .setDisplaySize(400, 100);
+
+    startButton.on("pointerover", function () {
+      this.setTint("0xffed8a");
+    });
+
+    startButton.on("pointerdown", function () {
+      this.setTint("0xffed8a");
+    });
+
+    startButton.on("pointerout", function () {
+      this.clearTint();
+    });
+
+    startButton.on("pointerup", function () {
+      this.clearTint();
+
+      alert("clicou");
+      //   sceneRef.scene.start("Game");
+    });
+  }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: MyGame
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: 800,
+  height: 600,
+  scene: MyGame,
 };
 
 const game = new Phaser.Game(config);
